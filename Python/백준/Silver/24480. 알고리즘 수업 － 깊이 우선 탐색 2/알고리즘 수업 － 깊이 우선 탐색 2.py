72a77543b4c7bc6bf5c40c97f1dea8ca58f8e112
Ahngab <102839564+Ahngab@users.noreply.github.com>
2025-02-07 23:59:21 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 584 ms, Memory: 70312 KB -BaekjoonHub

---
+++ Python/백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,29 @@
+import sys
+
+def DFS(node):
+    global time
+
+    time += 1
+    visited[node] = time
+    edges[node].sort(reverse=True)
+    for i in edges[node]:
+        if visited[i] == 0:
+            DFS(i)
+
+input = sys.stdin.readline
+sys.setrecursionlimit(150000)
+N, M, R = map(int, input().split())
+
+visited = [0]*(N+1)
+edges = [[] for _ in range(N+1)]
+time = 0
+
+for i in range(M):
+    a, b = map(int, input().split())
+    edges[a].append(b)
+    edges[b].append(a)
+
+
+DFS(R)
+for i in range(1, len(visited)):
+    print(visited[i])
