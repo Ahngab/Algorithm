44aa67faca4b95ab0c6717549ec547f313477894
Ahngab <102839564+Ahngab@users.noreply.github.com>
2025-02-08 22:19:56 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ Python/백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,19 +1,13 @@
 import sys
 
+input = sys.stdin.readline
 height = sorted([int(input()) for _ in range(9)])
 total = sum(height)
-flag = False
 
 for i in range(9):
-    for j in range(i,9):
+    for j in range(i+1,9):
         if total - height[i] - height[j] == 100:
-            No1 = i
-            No2 = j
-            flag = True
-            break
-    if flag:
-        break
-
-for i in range(len(height)):
-    if i != No1 and i != No2:
-        print(height[i])
+            for k in range(len(height)):
+                if k != i and k != j:
+                    print(height[k])
+            exit()
