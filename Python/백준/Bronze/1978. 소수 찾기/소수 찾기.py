b4fd70d138bf16697ee1831f38e27bb2b6a06558
Ahngab <102839564+Ahngab@users.noreply.github.com>
2025-02-08 12:25:39 +0900

    [Bronze II] Title: 소수 찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ Python/백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,18 +1,18 @@
 import sys
-N = int(sys.stdin.readline())
-flag = True
-data = list(map(int, sys.stdin.readline().rstrip().split()))
-result = []
 
-for i in data:
-    if i > 2:
-        for j in range(2, i):
-            if i % j == 0:
-                flag = False
-        if flag:
-            result.append(i)
-        else:
-            flag = True
-    elif i == 2:
-        result.append(i)
-print(len(result))
+limit = 1000
+prime = [0] * (limit + 1)
+prime[0:2] = [1,1]
+
+for i in range(2, limit + 1):
+    for j in range(2, limit//i + 1):
+        prime[i*j] = 1
+
+input = sys.stdin.readline
+N = int(input())
+cnt = 0
+test = list(map(int, input().split()))
+for i in test:
+    if prime[i] == 0:
+        cnt += 1
+print(cnt)
