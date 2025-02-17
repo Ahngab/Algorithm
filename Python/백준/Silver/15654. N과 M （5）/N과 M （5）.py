0a5171b9dab4d4514fa106bb92e8ee11436c7be7
Ahngab <102839564+Ahngab@users.noreply.github.com>
2025-02-17 10:39:11 +0900

    [Silver III] Title: N과 M (5), Time: 64 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ Python/백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -3,18 +3,5 @@ from itertools import permutations
 N, M = map(int, input().split())
 arr = list(map(str, sorted(list(map(int, input().strip().split())))))
 
-tmp = []
-    
-def perm(n, beforeString):
-    if n == M:
-        print(" ".join(beforeString))
-        return
-    
-    for i in range(N):
-        if arr[i] not in beforeString:
-            beforeString.append(arr[i])
-            perm(n + 1, beforeString)
-            beforeString.pop()
-
-
-perm(0, [])
+for i in permutations(arr, M):
+    print(" ".join(i))
