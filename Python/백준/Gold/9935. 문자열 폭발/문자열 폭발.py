03d1e7a78f4e6cc5f4e11ec21e768992eb016287
Ahngab <102839564+Ahngab@users.noreply.github.com>
2025-02-18 20:51:31 +0900

    [Gold IV] Title: 문자열 폭발, Time: 788 ms, Memory: 43596 KB -BaekjoonHub

--- Python/백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ Python/백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -1,21 +1,18 @@
-import sys
-from collections import deque
-
-input = sys.stdin.readline
 string = input().strip()
 bomb = input().strip()
-length = len(bomb)
+
+bombLength = len(bomb)
+stringLength = len(string)
 stack = []
 
-for i in string:
-    stack.append(i)
+for i in range(len(string)):
+    stack.append(string[i])
 
-    if ''.join(stack[len(stack)-length:len(stack)]) == bomb:
-        for _ in range(length):
+    if len(stack) >= bombLength and "".join(stack[-bombLength:]) == bomb:
+        for _ in range(bombLength):
             stack.pop()
-        
-if stack:
-    print(*stack, sep="")
 
+if stack:
+    print("".join(stack))
 else:
     print("FRULA")
