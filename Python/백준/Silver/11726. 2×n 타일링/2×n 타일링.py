c83cc95b97c84119c0fbaa1235f0a14422fe4ce6
Ahngab <102839564+Ahngab@users.noreply.github.com>
2025-02-24 15:31:33 +0900

    [Silver III] Title: 2×n 타일링, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

--- Python/백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ Python/백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,10 +1,10 @@
-from collections import deque
-
 N = int(input())
-dp = [0] * (N+1)
-dp[0:2] = [1, 1]
 
-for i in range(2, N+1):
-    dp[i] = dp[i-1] + dp[i-2]
+dp = [0] * (N + 1)
+dp[0], dp[1] = 1, 1
 
-print(dp[N]%10007)
+for i in range(2, N + 1):
+    dp[i] = dp[i - 1] + dp[i - 2]
+
+
+print(dp[N] % 10007)
