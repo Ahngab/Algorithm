c690e2ec1ca9452ea3cb1eb361a1f05b33ec3e10
Ahngab <102839564+Ahngab@users.noreply.github.com>
2025-02-24 21:34:36 +0900

    [Silver III] Title: 파도반 수열, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/9461. 파도반 수열/파도반 수열.py
+++ Python/백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -3,14 +3,11 @@ import sys
 input = sys.stdin.readline
 T = int(input())
 
+dp = [0] * (101)
+dp[:6] = [0, 1, 1, 1, 2, 2]
+for i in range(6, 101):
+    dp[i] = dp[i - 1] + dp[i - 5]
+
 for _ in range(T):
-    N = int(input())
-    arr = [0]* (N+4)
-
-    arr[1] = 1
-    arr[2] = 1
-    arr[3] = 1
-    for i in range(4, len(arr)):
-        arr[i] = arr[i-3] + arr[i-2]
-
-    print(arr[N])
+    n = int(input())
+    print(dp[n])
